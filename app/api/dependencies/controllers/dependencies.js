475e4a8d94d7a78fb5e5235220a83b8764c9d7de
Eric Hemmerlin <ehemmerlin@users.noreply.github.com>
2021-10-01 19:34:58 +0200

    WIP

--- app/api/dependencies/controllers/dependencies.js
+++ app/api/dependencies/controllers/dependencies.js
@@ -14,10 +14,77 @@ module.exports = {
     const entity = await strapi.services.dependencies.findOne({ url });
     return sanitizeEntity(entity, { model: strapi.models.dependencies });
   },
-  async webhook() {
+  async webhookGet() {
     const { data } = await axios.get('http://localhost:1337/dependencies/xcut');
 
     console.log(data);  
-    return data;
+    return "Received GET request. Check the console for more info.";
+  },
+  async webhookPost(ctx) {
+    console.log("Received webhook request to /webhook-receive");
+    console.log("Request body:");
+    console.log(ctx.request.body);
+
+    if (ctx.request.body.changes[0] && ctx.request.body.changes[0].type == "ADD" && ctx.request.body.changes[0].ref.type == "BRANCH") {
+        console.log("New branch created: "+ctx.request.body.changes[0].refId)
+
+        const refId = ctx.request.body.changes[0].refId;
+        const branch = refId.split('/')[2];
+
+        let headerGet = {
+            headers: {
+                'Authorization': 'Bearer ' + process.env.BITBUCKET_TOKEN
+            }
+          };
+    
+        axios.get('https://app:1337/dependencies/'+ctx.request.body.repository.slug)
+          .then(res => {
+            const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+            console.log('Status Code:', res.status);
+            console.log('Date in Response header:', headerDate);
+        
+            const projects = res.data;
+        
+            for(project of projects) {
+        
+                  console.log(JSON.stringify(project));
+  
+                  axios.get('http://bitbucket.plium.club:7990/rest/api/1.0/projects/'+project.url+'/commits?until=master', headerGet)
+                  .then(res => {
+                      console.log(res)
+
+                      let lastCommit = res.data.values[0].id;
+                      console.log("Last commit: " + lastCommit)
+
+                      var data = {
+                          name: branch,
+                          startPoint: lastCommit
+                      };
+
+                      let headerPost = {
+                          headers: {
+                              'Content-Type': 'application/json',
+                              'Authorization': 'Bearer ' + process.env.BITBUCKET_TOKEN
+                          }
+                      };
+
+                      axios.post('http://bitbucket.plium.club:7990/rest/api/1.0/projects/'+project.url+'/branches', data, headerPost)
+                      .then(res => {
+                          console.log(res)
+                      })
+                      .catch(error => {
+                          console.error(error.response)
+                      })
+
+                  })
+                  .catch(error => {
+                      console.error(error.response)
+                  })
+              }
+        });
+
+    }
+
+    return "Received POST request. Check the console for more info.";
   },
 };
