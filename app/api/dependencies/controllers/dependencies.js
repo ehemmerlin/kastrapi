00fbc44b9c3b2cde0c8a79f8b68160a3739b4a0c
Eric Hemmerlin <ehemmerlin@users.noreply.github.com>
2021-10-01 21:06:41 +0200

    WIP

--- app/api/dependencies/controllers/dependencies.js
+++ app/api/dependencies/controllers/dependencies.js
@@ -32,67 +32,61 @@ module.exports = {
     console.log(ctx.request.body);
 
     if (ctx.request.body.pullRequest && ctx.request.body.pullRequest.state == "MERGED" && ctx.request.body.pullRequest.open == false && ctx.request.body.pullRequest.closed == true) {
-        console.log("New pull request created: "+ctx.request.body.pullRequest.fromRef.id)
+      console.log("New pull request created: "+ctx.request.body.pullRequest.fromRef.id)
 
-        const refId = ctx.request.body.pullRequest.fromRef.id;
-        const branch = refId.split('/')[2];
+      const refId = ctx.request.body.pullRequest.fromRef.id;
+      const branch = refId.split('/')[2];
 
-        let headerGet = {
-            headers: {
-                'Authorization': 'Bearer ' + process.env.BITBUCKET_TOKEN
-            }
-          };
-        
-        console.log("Call api");
-        axios.get('http://bitbucket.plium.club:1337/dependencies/'+ctx.request.body.pullRequest.fromRef.repository.slug)
+      let headerGet = {
+          headers: {
+              'Authorization': 'Bearer ' + process.env.BITBUCKET_TOKEN
+          }
+        };
+      
+      console.log("Call api");
+      const { data } = axios.get('http://bitbucket.plium.club:1337/dependencies/'+ctx.request.body.pullRequest.fromRef.repository.slug);
+
+      console.log(data);  
+
+      const projects = res.projects;
+  
+      for(project of projects) {
+  
+          console.log(project);
+
+          axios.get('http://bitbucket.plium.club:7990/rest/api/1.0/projects/'+project.url+'/commits?until=master', headerGet)
           .then(res => {
-            //const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
-            //console.log('Status Code:', res.status);
-            //console.log('Date in Response header:', headerDate);
             //console.log(JSON.stringify(res));
 
-            const projects = res.projects;
-        
-            for(project of projects) {
-        
-                  console.log(JSON.stringify(project));
-  
-                  axios.get('http://bitbucket.plium.club:7990/rest/api/1.0/projects/'+project.url+'/commits?until=master', headerGet)
-                  .then(res => {
-                    //console.log(JSON.stringify(res));
+              let lastCommit = res.data.values[0].id;
+              console.log("Last commit: " + lastCommit)
 
-                      let lastCommit = res.data.values[0].id;
-                      console.log("Last commit: " + lastCommit)
+              var data = {
+                  name: branch,
+                  startPoint: lastCommit
+              };
 
-                      var data = {
-                          name: branch,
-                          startPoint: lastCommit
-                      };
+              let headerPost = {
+                  headers: {
+                      'Content-Type': 'application/json',
+                      'Authorization': 'Bearer ' + process.env.BITBUCKET_TOKEN
+                  }
+              };
 
-                      let headerPost = {
-                          headers: {
-                              'Content-Type': 'application/json',
-                              'Authorization': 'Bearer ' + process.env.BITBUCKET_TOKEN
-                          }
-                      };
+              axios.post('http://bitbucket.plium.club:7990/rest/api/1.0/projects/'+project.url+'/branches', data, headerPost)
+              .then(res => {
+                  console.log(res)
+              })
+              .catch(error => {
+                  console.error(error.response)
+              })
 
-                      axios.post('http://bitbucket.plium.club:7990/rest/api/1.0/projects/'+project.url+'/branches', data, headerPost)
-                      .then(res => {
-                          console.log(res)
-                      })
-                      .catch(error => {
-                          console.error(error.response)
-                      })
-
-                  })
-                  .catch(error => {
-                      console.error(error.response)
-                  })
-              }
-        });
-
+          })
+          .catch(error => {
+              console.error(error.response)
+          });
+        }
     }
-
     return "Received POST request. Check the console for more info.";
   },
 };
